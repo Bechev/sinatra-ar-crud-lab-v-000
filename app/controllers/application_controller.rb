4c47bcea6530f58ac5f5149eff01d7712ab9e77d
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-04-02 10:18:21 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,4 +43,10 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
+  delete '/posts/:id/delete' do #delete action
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    erb :deleted
+  end
+
 end
